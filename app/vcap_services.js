23e24c54c92ce06631e286b8297d25501e39a051
Mike Long <mlong@pivotal.io>
2016-04-15 14:28:47 -0700

    semicolons, eh?

--- app/vcap_services.js
+++ app/vcap_services.js
@@ -2,36 +2,36 @@
 module.exports = {
   get_service_label: function () {
     if (process.env.VCAP_SERVICES) {
-      var svc_info = JSON.parse(process.env.VCAP_SERVICES);
+      var svc_info = JSON.parse(process.env.VCAP_SERVICES)
       for (var label in svc_info) {
-        var svcs = svc_info[label];
+        var svcs = svc_info[label]
         for (var index in svcs) {
-          var label = svcs[index].label;
-          return label;
+          var label = svcs[index].label
+          return label
         }
       }
     }
   },
   get_service_name: function () {
     if (process.env.VCAP_SERVICES) {
-      var svc_info = JSON.parse(process.env.VCAP_SERVICES);
+      var svc_info = JSON.parse(process.env.VCAP_SERVICES)
       for (var label in svc_info) {
-        var svcs = svc_info[label];
+        var svcs = svc_info[label]
         for (var index in svcs) {
-          var label = svcs[index].name;
-          return label;
+          var label = svcs[index].name
+          return label
         }
       }
     }
   },
   get_service_plan: function () {
     if (process.env.VCAP_SERVICES) {
-      var svc_info = JSON.parse(process.env.VCAP_SERVICES);
+      var svc_info = JSON.parse(process.env.VCAP_SERVICES)
       for (var label in svc_info) {
-        var svcs = svc_info[label];
+        var svcs = svc_info[label]
         for (var index in svcs) {
-          var label = svcs[index].plan;
-          return label;
+          var label = svcs[index].plan
+          return label
         }
       }
     }
