5d40893b293c0b6cc838516de572181961876076
Mike Long <mlong@pivotal.io>
2016-04-19 16:58:14 -0700

    view app activity in the logs without having to manually refresh the web page

---
+++ public/javascripts/ping.js
@@ -0,0 +1,15 @@
+$(document).ready( function() {
+
+  var interval = 4000
+
+  var ping = setInterval( function() {
+    $.ajax({
+      url: '/ping.txt',
+      dataType: 'text',
+      success: function (data) {
+        $("[data-ping='ping']").append( data )
+      }
+    })
+    }, interval
+  )
+})
